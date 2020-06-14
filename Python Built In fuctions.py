00194d4a4ce0ffc43a5f9727f9988779d3d0cb80
krishna <bkesari@outlook.com>
2020-06-14 11:17:19 -0400

    commit

--- Python Built In fuctions.py
+++ Python Built In fuctions.py
@@ -1,3 +1,7 @@
 # First we will explore python's built in match functions
 
 # Standard libraries - built in functions & built in imported modules
+
+
+a = ((2*((1.01)**3)+2) - (2*((0.99)**3)+2))/0.02
+print(a)
