ef11000a14ae1c619c4b8749d9616b2f52eea850
krishna <bkesari@outlook.com>
2020-06-15 15:44:20 -0400

    commit

---
+++ Maths.py
@@ -0,0 +1,30 @@
+# Trigonometry, ceiling, floor and constants
+
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.nan)
+print(math.inf)
+
+# Trigonometry
+obstacle = math.cos(math.pi/4)
+print(obstacle)
+print(math.sin(math.pi/4))
+
+# Ceiling and floor
+cookies = 10.3
+candy = 7
+print(math.ceil(cookies))
+print(math.ceil(candy))
+
+age = 47.5
+print(math.floor(age))
+
+
+# Factorial, square root and GCD
+print(math.factorial(3))
+print(math.sqrt(64))
+
+## GCD (Greatest common denominator)
