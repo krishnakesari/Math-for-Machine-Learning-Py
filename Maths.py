aae9741170692f280ef224c265a7a58c2e2a2b09
krishna <bkesari@outlook.com>
2020-06-15 16:12:01 -0400

    commit

--- Maths.py
+++ Maths.py
@@ -83,7 +83,64 @@ print(statistics.variance(agesData))
 print(statistics.stdev(agesData))
 print(math.sqrt(statistics.variance(agesData)))
 
-#
+#Iteration tools
+import itertools
+
+## Infinite counting
+for x in itertools.count(50):
+    print(x)
+    if x == 80:
+        break;
+
+## Infinite Cycling
+for c in itertools.cycle("RACECAR"):
+    print(c)
+    x = x + 1
+    if x > 50:
+        break;
+
+## Infinite repeating
+for r in itertools.repeat(True):
+    print(r)
+    x = x + 1
+    if x == 100:
+        break;
+
+
+# Permutations and Combinations
+
+## Permutations
+election = {1: "B", 2:"A", 3:"C"}
+for p in itertools.permutations(election):
+    print(p)
+
+for p1 in itertools.permutations(election.values()):
+    print(p1)
+
+## Combinations
+colorsForPainting = ["Red", "Blue", "Yellow"]
+for c in itertools.combinations(colorsForPainting, 2):
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
