5b745b989d8fa333b17e5375fbddeff1d2c52454
Krishna Kesari <bkesari@outlook.com>
2020-06-20 09:53:15 -0400

    I/O

--- Python IO.py
+++ Python IO.py
@@ -1,2 +1,25 @@
 
 # Command line arguments
+import sys
+sys.argv
+print(len(sys.argv))
+
+print("Number of arguments:", len(sys.argv), ' arguments.')
+print("Arguments ", sys.argv)
+
+# Remove arguments
+# sys.argv.remove(sys.argv[0])
+
+# Setting up argument
+arguments = sys.argv
+sum = 2
+for arg in arguments:
+    try:
+        number = int(arg)
+        sum = sum + number
+    except Exception:
+        print("Bad Input")
+
+print(sum)
+
+
