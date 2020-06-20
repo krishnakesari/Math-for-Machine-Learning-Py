fb4648d60ec1ac5e469b87d39600d153d7175c69
Krishna Kesari <bkesari@outlook.com>
2020-06-20 10:06:46 -0400

    I/O

--- Python IO.py
+++ Python IO.py
@@ -27,5 +27,29 @@ print(sum)
 ## Output
 print("Hello")
 ## Input
-color = input("what is your favourite color")
-print(color)
+# color = input("what is your favourite color")
+# print(color)
+
+# Files and File Writing
+
+# Open a file
+myFile = open("scores.txt", "w")
+
+# w --> write
+# r --> read
+# r+ --> read and write
+# a --> append
+# Show attributes and properties of that file
+print("Name " + myFile.name)
+print("Mode " + myFile.mode)
+
+# Write to a file
+myFile.write("GBJ : 100\nKHD : 99\nBBB : 89")
+myFile.close()
+
+# Read the file
+myFile = open("scores.txt", "r")
+print("Reading..." + myFile.read(10)) # read only 10 characters
+print("Reading again" + myFile.read(10))
+
+
